5981e55f21e8af32bb47512c06fec96550d32e12
Hoon Wee <mrgravity817@gmail.com>
2023-07-16 01:02:19 +0900

    feat: add lager, rabbitmq specific dependency

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule BroadwayTickets.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:lager, :logger],
       mod: {BroadwayTickets.Application, []}
     ]
   end
